8d52d70eb9cfce102e22e8693fd2ea48255d7a4e
Dustin Oprea <myselfasunder@gmail.com>
2012-11-02 02:05:10 -0400

    Removed erroneous file.

--- snackwich/utility.py
+++
@@ -1,8 +0,0 @@
-
-from socket import inet_ntoa
-
-def ip_int_to_ascii(int_ip):
-    """Pad the integer IP before converting to ASCII."""
-
-    return inet_ntoa(hex(int(int_ip))[2:-1].zfill(8).decode('hex'))
-
