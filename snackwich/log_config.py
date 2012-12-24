14957f59556e161858af618f029bd47f176a4127
Dustin Oprea <myselfasunder@gmail.com>
2012-12-23 21:26:23 -0500

    We no longer log (was being duplicated).

--- snackwich/log_config.py
+++ snackwich/log_config.py
@@ -1,13 +1,14 @@
 import logging
 import logging.handlers
 
-default_logger = logging.getLogger()
-default_logger.setLevel(logging.DEBUG)
-
-log_syslog = logging.handlers.SysLogHandler('/dev/log')
-
-log_format = 'S-WICH: %(levelname)s %(message)s'
-log_syslog.setFormatter(logging.Formatter(log_format))
-
-default_logger.addHandler(log_syslog)
+#default_logger = logging.getLogger()
+#default_logger.setLevel(logging.DEBUG)
+#default_logger.setLevel(logging.INFO)
+#
+#log_syslog = logging.handlers.SysLogHandler('/dev/log')
+#
+#log_format = 'S-WICH: %(levelname)s %(message)s'
+#log_syslog.setFormatter(logging.Formatter(log_format))
+#
+#default_logger.addHandler(log_syslog)
 
